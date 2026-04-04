8ada43870a60fb754e02a5cc12842da8062929f4
simple <simple@server1.server1>
2026-04-04 13:57:18 +0500

    v1.1 work

--- app/schemas/response.py
+++ app/schemas/response.py
@@ -1,35 +1,40 @@
+# app/schemas/response.py
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+
 class BookItem(BaseModel):
     """Модель одной книги"""
-    title: str = Field(..., description="Название книги")
-    price: float = Field(..., description="Цена книги")
-    rating: int = Field(..., ge=1, le=5, description="Руйтинг от 1 до 5")
-    in_stock: bool = Field(..., description="Наличие книги")
+    title: str
+    price: float
+    rating: int = Field(..., ge=1, le=5)
+    in_stock: bool
 
-class SceapePageResponse(BaseModel):
-    status: str = Field(..., description="success или error")
+
+class ScrapePageResponse(BaseModel):
+    """Ответ для одной страницы"""
+    status: str
     page_url: str
     count: int
     items: List[BookItem]
-    error: Optional[str] = Field(None, description="Сообщение об ошибке, если status=error")
+    error: Optional[str] = None
+
 
 class MultiScrapeResponse(BaseModel):
     """Ответ для множественного парсинга"""
     status: str = "success"
     total_count: int
     page_count: int
-    execution_time: float = Field(..., description="Время выполнения в секундах")
+    execution_time: float
     results: List[ScrapePageResponse]
 
-class HelthResponse(BaseModel):
-    """Health check"""
+
+class HealthResponse(BaseModel):
+    """Health-check"""
     status: str = "ok"
 
 
 class ErrorResponse(BaseModel):
-    """Error Model"""
+    """Модель ошибки"""
     status: str = "error"
     detail: str
-
